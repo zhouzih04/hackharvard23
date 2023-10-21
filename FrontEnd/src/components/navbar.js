eeba8368bf3be278ac7e53a90d9c6cf3e857667b
Catherine <21260778c@gmail.com>
2023-10-21 09:00:00 -0400

    set up front end and worked on login page

---
+++ FrontEnd/src/components/navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { ButtonGroup, Button } from '@mui/material';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+
+import MenuIcon from '@material-ui/icons/Menu';
+import Box from '@mui/material/Box';
+
+function Navbar() {
+
+    return(
+        <div>
+            <p>this is a nav bar...i guess.</p>
+        </div>
+    );
+
+}
+
+export default Navbar;
